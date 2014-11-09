b937e32f825374840a5dd0655720b18a01718d21
Brad <braddeicide@hotmail.com>
2014-11-09 18:46:28 +1100

    Adding CentOS 6.6

--- scripts/centos/base.sh
+++ scripts/centos/base.sh
@@ -5,7 +5,7 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 cat > /etc/yum.repos.d/epel.repo << EOM
 [epel]
 name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/\$releasever/\$basearch
+baseurl=http://mirror.internode.on.net/pub/epel/\$releasever/\$basearch
 enabled=1
 gpgcheck=0
 EOM
