c3d2e07353400ff829a8df81ac2c03b2de846dd9
Brad <braddeicide@hotmail.com>
2015-07-06 12:03:21 +1000

    Hide output from ubuntu tty bug.
    CentOS 7 requires bzip2 for virtualbox
    
    Ignore .vagrant
    
    Extra YES for ntp
    
    Have a guess at python default location

--- scripts/ubuntu/base.sh
+++ scripts/ubuntu/base.sh
@@ -6,3 +6,6 @@ sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
+
+# https://github.com/mitchellh/vagrant/issues/1673
+sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
