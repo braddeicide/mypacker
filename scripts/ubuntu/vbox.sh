aa0671dba3d15a3623966083e9f97738606ddae8
brad <braddeicide@hotmail.com>
2015-07-13 10:14:42 +0000

    Clean better

--- scripts/ubuntu/vbox.sh
+++ scripts/ubuntu/vbox.sh
@@ -7,6 +7,7 @@ apt-get -y install --no-install-recommends libdbus-1-3
 /etc/init.d/virtualbox-ose-guest-utils stop
 rmmod vboxguest
 aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
+aptitude -y purge virtualbox-guest-x11 virtualbox-guest-dkms virtualbox-guest-utils
 aptitude -y install dkms
 
 # Install the VirtualBox guest additions
