8353339ba8a734cc8b4329ffe40921da985b6cfa
brad <braddeicide@hotmail.com>
2016-04-18 01:55:17 +1000

    Minor doco fix

--- repopulate_vagrant.py
+++ repopulate_vagrant.py
@@ -46,7 +46,7 @@ parser   = argparse.ArgumentParser()
 parser.add_argument('-t', '--type',      help='virtualbox, vmware, all'    , default='virtualbox')
 parser.add_argument('-p', '--parallel',  help='1 .. n'                     , default=1)
 parser.add_argument('-P', '--path',      help='Directory to append to path', default='')
-parser.add_argument('-s', '--silent',    help='1 .. n'                     , action="store_true")
+parser.add_argument('-s', '--silent',    help=''                           , action="store_true")
 parser.add_argument('-l', '--logtofile', help='will create machine.log'    , action="store_true")
 parser.add_argument('-m', '--matching',  help='build machines matching'    , default='*')
 
