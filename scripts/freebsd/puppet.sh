5e6b68748ed395e9c1cb909f7af4d246c4d1cbc1
brad <braddeicide@hotmail.com>
2016-04-23 22:31:30 +1000

    Change for FreeBSD 10.3, and depricate non LTS Ubuntu

--- scripts/freebsd/puppet.sh
+++ scripts/freebsd/puppet.sh
@@ -6,6 +6,8 @@ freebsd_major=`uname -r | awk -F. '{ print $1 }'`
 if [ $freebsd_major -gt 9 ]; then
   # Use pkgng
   pkg install -y puppet
+  # puppet37||puppet38||puppet4
+  pkg install -y puppet4
 else
   # Use old pkg
   pkg_add -r puppet 
